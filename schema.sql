a77c74b4558a0b78165c6d21c8d03a1b8dc2ef6d
Jesse Weinstein <jesse@wefu.org>
2016-04-04 23:11:14 -0700

    Adjust naming

--- schema.sql
+++ schema.sql
@@ -35,10 +35,10 @@ CREATE TABLE IF NOT EXISTS response_hashes (
 CREATE UNIQUE INDEX response_to_hash ON response_hashes (response_id, hash_id);
 CREATE UNIQUE INDEX hash_to_response ON response_hashes (hash_id, response_id);
 
-CREATE TABLE IF NOT EXISTS request_wrappers (
+CREATE TABLE IF NOT EXISTS wrapped_inner_requests (
 	rw_id INTEGER PRIMARY KEY,
-	wrapper_response_id INTEGER NOT NULL REFERENCES responses (response_id),
+	wrapped_response_id INTEGER NOT NULL REFERENCES responses (response_id),
 	inner_request_id INTEGER NOT NULL REFERENCES requests (request_id)
 );
-CREATE UNIQUE INDEX wrapper_to_inner ON request_wrappers (wrapper_response_id, inner_request_id);
-CREATE UNIQUE INDEX inner_to_wrapper ON request_wrappers (inner_request_id, wrapper_response_id);
+CREATE UNIQUE INDEX wrapped_to_inner ON wrapped_inner_requests (wrapped_response_id, inner_request_id);
+CREATE UNIQUE INDEX inner_to_wrapped ON wrapped_inner_requests (inner_request_id, wrapped_response_id);
